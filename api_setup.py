cfd1558f32d1fda8a08c88032c3ebadf0c5168e0
kaufmak2 <kaufmak2@gmail.com>
2017-03-19 21:16:58 -0700

    Delete api_setup.py

--- api_setup.py
+++
@@ -1,8 +0,0 @@
-import configparser
-config = configparser.ConfigParser()
-config['DEFAULT'] = {}
-config['DEFAULT']['api_key'] = '800c934905374c26ccf15885be69965e'
-config['DEFAULT']['sql_user'] = 'root'
-config['DEFAULT']['sql_key'] = 'celtics'
-with open('config.ini', 'w') as configfile:
-	config.write(configfile)
